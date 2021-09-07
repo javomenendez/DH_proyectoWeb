9b1c6a9862564f29d94fd8d2f62419742b850ea6
javomenendez <javier.menendez@live.com.ar>
2021-09-07 17:50:22 -0300

    Arreglos de desafios #2

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const publicPath = path.resolve(__dirname, "./public");
+const publicPath = path.resolve(__dirname, "../public");
 app.use(express.static(publicPath));
 
 app.listen(3000, ()=>console.log("Servidor corriendo en puerto 3000"));
 
-app.get("/", (req,res)=>res.sendFile(path.join(__dirname,"./views/home.html")));
+app.get("/", (req,res)=>res.sendFile(path.join(__dirname,"../views/home.html")));
