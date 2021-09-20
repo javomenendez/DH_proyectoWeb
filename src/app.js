8c2ab64601f0a04f2ddacd9fd7162ac2593efce9
javomenendez <javier.menendez@live.com.ar>
2021-09-20 17:24:58 -0300

    Creacion login.html y register.html

--- src/app.js
+++ src/app.js
@@ -6,8 +6,16 @@ const publicPath = path.resolve(__dirname, "../public");
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => console.log("Servidor corriendo en puerto 3000"));
-
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "../views/home.html"))
 );
+
+app.get("/registro", (req, res) =>
+  res.sendFile(path.join(__dirname, "../views/register.html"))
+);
+
+app.get("/login", (req, res) =>
+  res.sendFile(path.join(__dirname, "../views/login.html"))
+);
+
+app.listen(3000, () => console.log("Servidor corriendo en puerto 3000"));
