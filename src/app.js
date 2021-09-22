f54616d6d9c73edb00ce6fd2e7231528f1773375
javomenendez <javier.menendez@live.com.ar>
2021-09-22 12:31:16 -0300

    Se agrego puertos y archivo PROCFILE para subir proyecto a Heroku

--- src/app.js
+++ src/app.js
@@ -18,4 +18,6 @@ app.get("/login", (req, res) =>
   res.sendFile(path.join(__dirname, "../views/login.html"))
 );
 
-app.listen(3000, () => console.log("Servidor corriendo en puerto 3000"));
+app.listen(process.env.PORT || 3000, () =>
+  console.log("Servidor corriendo en puerto 3000")
+);
